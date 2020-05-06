8cb191c20cbdf88c80415f8dffc36481db0c7555
Tien Phan <tienphan1989@gmail.com>
2020-05-06 23:13:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ def nyc_pigeon_organizer(data)
   new_data = {}
   data.each do |key, value|
     #color is key, value is what color
-    
+    value.each do |array, name|
+      #array is group of names, name is pigeon name
+      
   
   end
   
