635568096fb31382b5678b6e2ba3f296789df573
Tien Phan <tienphan1989@gmail.com>
2020-05-06 23:18:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,13 @@ def nyc_pigeon_organizer(data)
   new_data = {}
   data.each do |key, value|
     #color is key, value is what color
-    value.each do |array, name|
-      #array is group of names, name is pigeon name
-      
-  
+    value.each do |category, array|
+      #category is color/gender/lives is group of names, array is group of names
+      array.each do |name|
+        new_data[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
   end
-  
 end
+  
+
